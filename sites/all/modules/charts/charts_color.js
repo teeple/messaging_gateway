d5ae36f08af0a254f138195cec15847239e55428
newbiz <newbiz.uangel@gmail.com>
2012-03-13 18:05:47 +0900

    modify theme for demonstration

--- sites/all/modules/charts/charts_color.js
+++ sites/all/modules/charts/charts_color.js
@@ -3,24 +3,28 @@
  * @file
  * Set the color palette to Charts.
  */
-Drupal.behaviors.chartsColor = function (context) {
-  var chartsColor = function() {
-    var colors = $("#edit-color-color-palette").val().split(",");
-    var series = ["background", "text", 0, 1, 2, 3, 4, 5, 6, 7];
-    var disabled = ($("#edit-color-color-palette").children("option:selected").text() == Drupal.settings.chartsColorCustom);
-    for (var s = 0, len = series.length; s < len; ++s) {
-      if (colors[s]) {
-        $("#edit-color-"+ series[s]).val(colors[s]);
-      }
-      if (disabled) {
-        $("#edit-color-"+ series[s]).removeAttr("readonly");
-      }
-      else {
-        $("#edit-color-"+ series[s]).attr("readonly", "readonly");
-      }
-    }
-  };
+(function ($) {
+    Drupal.behaviors.chartsColor = {
+        attach: function (context) {
+            var chartsColor = function() {
+                var colors = $("#edit-color-color-palette").val().split(",");
+                var series = ["background", "text", 0, 1, 2, 3, 4, 5, 6, 7];
+                var disabled = ($("#edit-color-color-palette").children("option:selected").text() == Drupal.settings.chartsColorCustom);
+                for (var s = 0, len = series.length; s < len; ++s) {
+                    if (colors[s]) {
+                        $("#edit-color-"+ series[s]).val(colors[s]);
+                    }
+                    if (disabled) {
+                        $("#edit-color-"+ series[s]).removeAttr("readonly");
+                    }
+                    else {
+                        $("#edit-color-"+ series[s]).attr("readonly", "readonly");
+                    }
+                }
+            };
 
-  $("#edit-color-color-palette").change(chartsColor);
-  chartsColor();
-};
+            $("#edit-color-color-palette").change(chartsColor);
+            chartsColor();
+        }
+    };
+})(jQuery);
